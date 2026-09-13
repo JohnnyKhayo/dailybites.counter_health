05019d4b98c158456a48b26cf7873d19005cb2ed
khayo <khayojohnstone7@gmail.com>
2026-09-13 22:09:57 +0300

    feat: select form elements in script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+
+ const foodForm = document.getElementById("food-form");
+const foodNameInput = document.getElementById("food-name");
+const caloriesInput = document.getElementById("calories");
+const foodList = document.getElementById(food-list);
+const totalCalories = document.getElementById("total-calories");
+const resetButton = document.getElementById("reset-btn")
+const lookupButton = document.getElementById("lookup-btn");
+const lookupMessage = document.getElementById("lookup-message")
+
+let foods = [];
