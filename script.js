5cd1d0362e9380589916adc45f7573b70c3907de
khayo <khayojohnstone7@gmail.com>
2026-09-13 22:17:00 +0300

    refactor: select form elements in script.js section

--- script.js
+++ script.js
@@ -2,11 +2,11 @@
  const foodForm = document.getElementById("food-form");
 const foodNameInput = document.getElementById("food-name");
 const caloriesInput = document.getElementById("calories");
-const foodList = document.getElementById(food-list);
+const foodList = document.getElementById("food-list");
 const totalCalories = document.getElementById("total-calories");
-const resetButton = document.getElementById("reset-btn")
+const resetButton = document.getElementById("reset-btn");
 const lookupButton = document.getElementById("lookup-btn");
-const lookupMessage = document.getElementById("lookup-message")
+const lookupMessage = document.getElementById("lookup-message");
 
 let foods = [];
 
